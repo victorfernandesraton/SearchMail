bae60aa45927ee23eb5937b79ec1afb51d14b5cd
victorfernandesraton <43411882+victorfernandesraton@users.noreply.github.com>
2019-03-21 16:01:32 -0300

    sql

--- index.php
+++ index.php
@@ -3,16 +3,17 @@
     require_once "./_fun/_ifvoid.php";
     require_once "./_fun/_scanMail.php";
     // conectando ao sql
-    // $pdo = new cx_bench("root","vfbr1101","mysql","localhost","bteste","3306");
-    // $cx = $pdo->cx_bench();
+    $pdo = new cx_bench("root","vfbr1101","mysql","localhost","bteste","3306");
+    $cx = $pdo->cx_bench();
 
     // verificando existência
-    // $query = "SHOW TABLES";
-    // $stmt = $cx->prepare($query);
-    // $stmt->execute();
-    // $out = $stmt->fetch(PDO::FETCH_ASSOC);
-    // IfVoid($out);
+    $query = "SHOW TABLES";
+    $stmt = $cx->prepare($query);
+    $stmt->execute();
+    $out = $stmt->fetch(PDO::FETCH_ASSOC);
+    IfVoid($out);
     $scan = new ScMail("victor.baiao1101@gmail.com");
+    var_dump(($scan->getPosition()));
     var_dump(($scan->getBefore()));
     var_dump(($scan->getAfter()));
 
