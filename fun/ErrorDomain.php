5283af2aadbca099bd220bac2cedcd40c5e77ecb
victorfernandesraton <43411882+victorfernandesraton@users.noreply.github.com>
2019-03-26 22:04:28 -0300

    enxuto

--- fun/ErrorDomain.php
+++ fun/ErrorDomain.php
@@ -1,23 +1,23 @@
 <?php
 
 function ErrorDomain($mail,$domain,$error) {
-$before = 0;
-if ($mail->getStatus() == false) { // se o estatus for falso:
-    if (strlen($mail->getDomain()) < 4) { // em casos onde temos endereço de email com poucos caracteres
-        $mail->setSimilarDomain(false);
-    } else {
-        similar_text($mail->getDomain(),$domain->getDomain(),$before);
-        if($before == $mail->getPercent() && $before != 0) { // caso onde há duas possibilidades distintas de semelhança
-            foreach ($error as $error_value) {
-                if (strcmp($mail->getSimilarDomain(),$error_value) == 0 || strcmp($domain->getDomain(),$error_value) == 0) {
-                    $mail->setSimilarDomain($error_value);
+    $before = 0;
+    if ($mail->getStatus() == false) { // se o estatus for falso:
+        if (strlen($mail->getDomain()) < 4) { // em casos onde temos endereço de email com poucos caracteres
+            $mail->setSimilarDomain(false);
+        } else {
+            similar_text($mail->getDomain(),$domain->getDomain(),$before);
+            if($before == $mail->getPercent() && $before != 0) { // caso onde há duas possibilidades distintas de semelhança
+                foreach ($error as $error_value) {
+                    if (strcmp($mail->getSimilarDomain(),$error_value) == 0 || strcmp($domain->getDomain(),$error_value) == 0) {
+                        $mail->setSimilarDomain($error_value);
+                    }
                 }
+            } else if ($before > $mail->getPercent() && $mail->getStatus() == false) { // detecta qual tem a maior porcentagem
+                $mail->setPercent($before);
+                $mail->setSimilarDomain($domain->getDomain());
             }
-        } else if ($before > $mail->getPercent() && $mail->getStatus() == false) { // detecta qual tem a maior porcentagem
-            $mail->setPercent($before);
-            $mail->setSimilarDomain($domain->getDomain());
-        }
-    }            
-}
+        }            
+    }
 }
 ?>
