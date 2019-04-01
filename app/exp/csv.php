df19fd257452afe8e802ee892c9d9b2c3a3972a8
victorfernandesraton <43411882+victorfernandesraton@users.noreply.github.com>
2019-04-01 16:28:30 -0300

    mysql maker

---
+++ app/exp/csv.php
@@ -0,0 +1,49 @@
+<?php
+$path = dirname(__DIR__);
+require_once $path."../../database/_sql_connect.php";
+
+// header que gera cvs
+header('Content-type: application/ms-excel');
+header('Content-Disposition: attachment; filename=sample.csv');
+
+$cx = cx_bench("mailtool");
+if (verfy_tb("mailcorrect") == true) {
+    $query = "SELECT * FROM mailcorrect";
+    $stmt = $cx->prepare($query);
+    $stmt->execute();
+    $mailcorrect_list = $stmt->fetchALL(PDO::FETCH_ASSOC);
+} else {
+    $query = "CREATE TABLE mailcorrect (
+        index int(11) NOT NULL AUTO_INCREMENT,
+        mailAdress varchar(256) COLLATE utf8_bin NOT NULL,
+        region varchar(5) COLLATE utf8_bin NOT NULL,
+        user varchar(256) COLLATE utf8_bin NOT NULL COMMENT 'Até antes do arroba',
+        PRIMARY KEY (index)
+      ) ENGINE=InnoDB AUTO_INCREMENT=8071 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+      ";
+    $sql_mailcorrect = $cx->prepare($query);
+    
+    // verificação de uso
+    $sql_mailcorrect->execute();
+
+    // query sql tabela final
+    $query = "SELECT * FROM mailcorrect";
+    $sql_mailcorrect = $cx->prepare($query);
+    
+    // verificação de uso
+    $sql_mailcorrect->execute();
+    
+    // array de valores
+    $mailcorrect_list = $sql_mailcorrect->fetchALL(PDO::FETCH_ASSOC);
+}
+
+$html = null;
+
+foreach ($mailcorrect_list as $value) {
+    if ($value['user'] != null || $value['user'] != "") {
+        $html .= $value['user']."@".$value['mailAdress']."\n";
+    }
+}
+
+echo $html;
+?>
--- app/export.php
+++ app/export.php
@@ -17,7 +17,7 @@
     <div class="tap-target bg-y" data-target="menu">
         <div class="tap-target-content">
             <h5 class="ftb">Exportar tabelas</h5>
-            <p class="ftb">Nesta parte é possível exportar os endereços corrigidos em MYSQL ou em cvs</p>
+            <p class="ftb">Nesta parte é possível exportar os endereços corrigidos em em .CSV</p>
             <p class="ftb">OBS: alguns casos podem demorar</p>
         </div>
     </div>
@@ -30,8 +30,7 @@
     <div class="container">
         <h1>Exportar tabelas</h1>
         <h2>OBS: caso seja iniciada a exportação recomenda-se que o usuário não ultilize a ferramenta até que a exportação esteja concluida</h2>
-        <a class="btn" href="./exp/sql/php">Exportar em MySQL</a>
-        <a class="btn" href="./exp/sql/php">Exportar em CSV</a>
+        <a class="btn" href="../app/exp/csv.php">Exportar em CSV</a>
     </div>
     <?php require_once $path."/config/footer.php"; ?>
     <script type="text/javascript" src="./js/getForm.js"></script>
